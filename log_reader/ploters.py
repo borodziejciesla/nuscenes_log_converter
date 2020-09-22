Fix orientation (quaternions to Euler)

log_reader/ploters.py
@@ -4,6 +4,7 @@ from log_reader.log_reader import LogReader
 import os
 import numpy as np
 import time
+import scipy.linalg
 
 class Ploter:
     def __init__(self, detections_logs_list=[], objects_log_list=[], host_data_log=''):
@@ -25,10 +26,6 @@ class Ploter:
             self.__host_data_reader = LogReader(self.__host_data_log)
 
     def PlotDetections(self, scan_data):
-        x = []
-        y = []
-        z = []
-
         yaw_rad = np.deg2rad(scan_data.sensor_origin.yaw)
         pitch_rad = np.deg2rad(scan_data.sensor_origin.pitch)
         roll_rad = np.deg2rad(scan_data.sensor_origin.roll)
@@ -42,21 +39,56 @@ class Ploter:
         Rz = np.array([[cy, -sy, 0], [sy, cy, 0], [0, 0, 1]])
         origin_offset = np.array([scan_data.sensor_origin.x,  scan_data.sensor_origin.y, scan_data.sensor_origin.z])
 
+        detections_number = len(scan_data.detections)
+        
+        distance = np.zeros((detections_number, 1))
+        azimuth = np.zeros((detections_number, 1))
+        range_rate = np.zeros((detections_number, 1))
+
+        x = np.zeros((detections_number, 1))
+        y = np.zeros((detections_number, 1))
+        z = np.zeros((detections_number, 1))
+
+        i = 0
+
         for detection in scan_data.detections:
             X = np.array([detection.x, detection.y, detection.z])
             x_rot = Rx @ Ry @ Rz @ X + origin_offset
-            x.append(x_rot[0])
-            y.append(x_rot[1])
-            z.append(x_rot[2])
+            x[i] = x_rot[0]
+            y[i] = x_rot[1]
+            z[i] = x_rot[2]
+
+            distance[i] = detection.range
+            azimuth[i] = detection.azimuth
+            range_rate[i] = detection.range_rate
+
+            i += 1
+
         plt.plot(x, y, 'r.')
         plt.axis('equal')
         plt.grid(True)
         plt.show()
-        #time.sleep(0.2)
-        #plt.close()
+        time.sleep(0.2)
+        plt.close()
 
     def PlotObject(self, scan_data):
         pass
 
     def PlotHostData(self, scan_data):
         pass
+
+
+def FindVelocity(range_rate, azimuth):
+    l = len(range_rate)
+
+    H = np.zeros((l, 2))
+    Y = np.zeros((l, 1))
+
+    for idx in range(l):
+        H[idx, 0] = np.cos(azimuth[idx])
+        H[idx, 1] = np.sin(azimuth[idx])
+
+        Y[idx] = -range_rate[idx]
+
+    v = scipy.linalg.pinv(H.transpose() @ H) @ H.transpose() @ Y
+    return v

log_writer/scan_readers.py
@@ -2,6 +2,8 @@ import abc
 from nuscenes.utils.data_classes import RadarPointCloud, LidarPointCloud
 from dataclasses import dataclass
 from math import sqrt, atan2, hypot
+from pyquaternion import Quaternion
+from scipy.spatial.transform import Rotation
 
 @dataclass()
 class Origin:
@@ -127,9 +129,7 @@ class BaseSensorScanReader:
         y = calibrated_sensor['translation'][1]
         z = calibrated_sensor['translation'][2]
 
-        yaw = calibrated_sensor['rotation'][2]
-        pitch = calibrated_sensor['rotation'][1]
-        roll = calibrated_sensor['rotation'][0]
+        yaw, pitch, roll = Quaternion2YPR(calibrated_sensor['rotation'])
 
         return Origin(x, y, z, yaw, pitch, roll)
 
@@ -236,11 +236,9 @@ class VisionScanReader(BaseSensorScanReader):
         v_z = 0
         v_z_std = 0
 
-        yaw = object_raw.orientation[0]
+        yaw, pitch, roll = Quaternion2YPR(object_raw.orientation)
         yaw_std = 0
-        pitch = object_raw.orientation[1]
         pitch_std = 0
-        roll = object_raw.orientation[2]
         roll_std = 0
 
         yaw_rate = 0
@@ -274,8 +272,11 @@ class HostDataReader:
         y = raw_ego_data['translation'][1]
         z = raw_ego_data['translation'][2]
 
-        yaw = raw_ego_data['rotation'][2]
-        pitch = raw_ego_data['rotation'][1]
-        roll = raw_ego_data['rotation'][0]
+        yaw, pitch, roll = Quaternion2YPR(raw_ego_data['rotation'])
 
         return HostData(time_stamp, x, 0.0, y, 0.0, z, 0.0, yaw, 0.0, pitch, 0.0, roll, 0.0)
+
+def Quaternion2YPR(q):
+    rot = Rotation.from_quat([q[1], q[2], q[3], q[0]])
+    euler = rot.as_euler('xyz', degrees=False)
+    return euler[2], euler[1], euler[0]

test_readers.py
@@ -1,10 +1,11 @@
 from log_reader.log_reader import LogReader
 import numpy as np
 from log_reader.ploters import Ploter
+import matplotlib.pyplot as plt
 
 # Radar Data
-reader = LogReader("D:\\GIT\\SLAM\\data_set\\scene-0061_RADAR_BACK_LEFT.rad")
-ploter = Ploter(['D:\\GIT\\SLAM\\data_set\\scene-0061_RADAR_BACK_LEFT.rad'])
+reader = LogReader("/home/maciek/Downloads/output/scene-0061_RADAR_FRONT.rad")
+ploter = Ploter(['/home/maciek/Downloads/output/scene-0061_RADAR_FRONT.rad'])
 
 timestamps = []
 
@@ -28,16 +29,35 @@ if reader.IsFileOpen():
 reader.CloseFile()
 
 # Host Data
-reader = LogReader("D:\\GIT\\SLAM\\data_set\\scene-0103_HOST_DATA.host")
+reader = LogReader("/home/maciek/Downloads/output/scene-0103_HOST_DATA.host")
 
 timestamps = []
 
 if reader.IsFileOpen():
     scans_number = reader.GetScansNumber()
+
+    x = np.zeros((scans_number, 1))
+    y = np.zeros((scans_number, 1))
+    z = np.zeros((scans_number, 1))
+    yaw = np.zeros((scans_number, 1))
+    pitch = np.zeros((scans_number, 1))
+    roll = np.zeros((scans_number, 1))
+
     for index in range(0, scans_number):
         scan_data = reader.GetScanByIndex(index)
+        x[index] = scan_data.x
+        y[index] = scan_data.y
+        z[index] = scan_data.z
+        yaw[index] = scan_data.yaw
+        pitch[index] = scan_data.pitch
+        roll[index] = scan_data.roll
         print("\tts = {0:.2f}".format(scan_data.time_stamp))
         timestamps.append(scan_data.time_stamp)
+
+    plt.plot(pitch)
+    #plt.axis('equal')
+    plt.grid(True)
+    plt.show()
 
     print("Get By Timestamp")
     scan_data = reader.GetScanByTime(1533151621041512)